8de8b85b91eb6856cb4486adfc0f813961801e93
Manuel Bogner <manuel.bogner@openresearch.com>
2024-11-04 01:24:37 +0100

    extend release with publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,15 @@ tasks {
         archiveClassifier.set("javadoc")
         from(named<org.jetbrains.dokka.gradle.DokkaTask>("dokkaJavadoc").get().outputDirectory)
     }
+
+    named("afterReleaseBuild") {
+        dependsOn(
+            "signMavenPublication",
+            "publishToMavenLocal",
+            "publishToSonatype",
+            "closeAndReleaseSonatypeStagingRepository"
+        )
+    }
 }
 
 sonarqube {
